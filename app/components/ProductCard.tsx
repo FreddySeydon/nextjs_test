1b0e80292170c0a4077ff88ce7a5fba9b7426715
Renja <renjaschmakeit@gmail.com>
2023-12-10 09:11:08 +0100

    Tried tailwind inside the ProductCard component

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart'
-import styles from './ProductCard.module.css'
+// import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
-      Product Card Component
+    <div className='p-5 my-5 bg-teal-300 text-white text-xl hover:bg-teal-500 cursor-pointer'>
       <AddToCart />
     </div>
   )
